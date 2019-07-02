d631d7f965db36faf3267a89cfd764d5581452ef
CJDaniel96 <cjtsai96@gmail.com>
2019-07-03 01:08:18 +0800

    Boston_Housing

--- keras/Reuters.py
+++ keras/Reuters.py
@@ -11,7 +11,9 @@ Reuters
 from keras.datasets import reuters
 import numpy as np
 from keras.utils import to_categorical
-from keras import models, layers, optimizers, losses, metrics
+from keras import models, layers, optimizers, losses, metrics, activations
+import matplotlib.pyplot as plt
+import copy
 
 (train_data, train_labels), (test_data, test_labels) = reuters.load_data(num_words=10000)
 
@@ -60,3 +62,55 @@ history = model.fit(partial_x_train,
                     epochs=20,
                     batch_size=512,
                     validation_data=(x_val, y_val))
+
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+epochs = range(1, len(loss) + 1)
+plt.plot(epochs, loss, 'r', label='Training loss')
+plt.plot(epochs, val_loss, 'b', label='Validation loss')
+plt.title('Training and validation loss')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
+
+plt.show()
+
+plt.clf()
+
+acc = history.history['categorical_accuracy']
+val_acc = history.history['val_categorical_accuracy']
+plt.plot(epochs, acc, 'r', label='Training acc')
+plt.plot(epochs, val_acc, 'b', label='Validation acc')
+plt.title('Training and validation accuracy')
+plt.xlabel('Epochs')
+plt.ylabel('Accuracy')
+plt.legend()
+
+plt.show()
+
+# epochs = 9
+model = models.Sequential() 
+model.add(layers.Dense(64, activation=activations.relu, input_shape=(10000,)))
+model.add(layers.Dense(64, activation=activations.relu))
+model.add(layers.Dense(46, activation=activations.softmax))
+
+model.compile(optimizer=optimizers.rmsprop(lr=0.001),
+              loss=losses.categorical_crossentropy,
+              metrics=[metrics.categorical_accuracy])
+
+model.fit(partial_x_train,
+          partial_y_train,
+          epochs=9,
+          batch_size=512,
+          validation_data=(x_val, y_val))
+
+results = model.evaluate(x_test, one_hot_test_labels)
+print(results)
+
+test_labels_copy = copy.copy(test_labels)
+np.random.shuffle(test_labels_copy)
+hits_array = np.array(test_labels) == np.array(test_labels_copy)
+print(float(np.sum(hits_array))/len(test_labels))
+
+predictions = model.predict(x_test)
+print(np.argmax(predictions[0]))
